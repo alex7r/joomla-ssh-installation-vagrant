2230eb12ebc7a912895e75e6966920fa8cfaa2cf
TsaiEvgenii <tsai.evgenii@gmail.com>
2017-04-10 16:26:41 +0300

    Update fix_htaccess.sh

--- fix_htaccess.sh
+++ fix_htaccess.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cd /var/www
+cd /var/www/html
 sed -i "s/## End - Joomla! core SEF Section./## End - Joomla! core SEF Section.\nphp_value upload_max_filesize 8M/" htaccess.txt
 mv htaccess.txt .htaccess
